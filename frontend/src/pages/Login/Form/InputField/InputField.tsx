60ea26725745ca038361b612d287f9d0b329b93c
Havock48 <adit.github@gmail.com>
2024-01-16 23:48:08 +0100

    InputField type added

--- frontend/src/pages/Login/Form/InputField/InputField.tsx
+++ frontend/src/pages/Login/Form/InputField/InputField.tsx
@@ -1,7 +1,16 @@
 // SCSS
 import styles from './InputField.module.scss';
 
-export default function InputField({ value, type, id, className, autoComplete, handleChange }: any) {
+type InputFieldType = {
+    value: string,
+    type: string,
+    id: string,
+    className: string,
+    autoComplete: string,
+    handleChange: (value: string, id: string) => void
+}
+
+export default function InputField({ value, type, id, className, autoComplete, handleChange } : InputFieldType) {
     return (
         <div className={styles.inputContainer}>
             <label htmlFor={id}>{value} *</label>
