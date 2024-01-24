26cd9e8619b2903c162b944738a727c813e1da26
aditokmoo <adit.github@gmail.com>
2024-01-24 17:43:33 +0100

    icons added on dashboard

--- frontend/src/pages/UserProfile/UserProfile.tsx
+++ frontend/src/pages/UserProfile/UserProfile.tsx
@@ -1,8 +1,13 @@
 import { Link } from 'react-router-dom'
 import { useGetUser } from '../../hooks/useAuth'
+import Header from '../../components/Header/Header'
+// React icons
+import { LiaBoxOpenSolid } from "react-icons/lia";
+import { IoLocationOutline } from "react-icons/io5";
+import { FaRegUser, FaRegHeart  } from "react-icons/fa6";
+import { CiLogout } from "react-icons/ci";
 // SCSS
 import styles from './UserProfile.module.scss'
-import Header from '../../components/Header/Header'
 
 export default function UserProfile() {
   const { data, isLoading } = useGetUser()
@@ -38,18 +43,23 @@ export default function UserProfile() {
               </p>
               <div className={styles.sideBarOptions}>
                 <div className={styles.option}>
+                  <LiaBoxOpenSolid className={styles.icon}/>
                   <h3>Orders</h3>
                 </div>
                 <div className={styles.option}>
+                  <IoLocationOutline className={styles.icon}/>
                   <h3>Address</h3>
                 </div>
                 <div className={styles.option}>
-                  <h3>Account details</h3>
-                </div>
-                <div className={styles.option}>
+                  <FaRegHeart className={styles.icon}/>
                   <h3>Wishlist</h3>
                 </div>
                 <div className={styles.option}>
+                  <FaRegUser className={styles.icon}/>
+                  <h3>Account details</h3>
+                </div>
+                <div className={styles.option}>
+                  <CiLogout className={styles.icon}/>
                   <h3>Logout</h3>
                 </div>
               </div>
